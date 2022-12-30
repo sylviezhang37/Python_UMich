6fb5fe8d5206f652fa86b94cf339aa6b5f29398f
sylviezhang37 <114536986+sylviezhang37@users.noreply.github.com>
2022-12-30 08:02:33 -0500

    Update ex_11.py

--- Course 3: Using Python to Access Web Data/ex_11.py
+++ Course 3: Using Python to Access Web Data/ex_11.py
@@ -6,24 +6,23 @@
 #We provide two files for this assignment.
 #One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
 
-#Sample data: http://py4e-data.dr-chuck.net/regex_sum_42.txt (There are 90 values with a sum=445833)
-#Actual data: http://py4e-data.dr-chuck.net/regex_sum_38794.txt (There are 90 values and the sum ends with 360)
+#Actual data: http://py4e-data.dr-chuck.net/regex_sum_38794.txt 
 
 #The basic outline of this problem is to read the file, look for integers using the re.findall(),
 #looking for a regular expression of '[0-9]+'
 #and then converting the extracted strings to integers and summing up the integers.
 
 import re
+file = open('regex_sum_38794.txt')
+sum=0
 
-name = input("Enter file:")
+for line in file:
+    line = line.rstrip()
+    # print(line)
+    output = re.findall('[0-9]+',line)
+    if len(output) > 0:
+        # print(output)
+        for number in output:
+            sum = sum + int(number)
 
-if len(name) < 1 : name = "test.txt"
-fh = open(name)
-newlist = list()
-
-for line in fh :
-    line = re.findall('[0-9]+', line)  #finds all numbers '.[0-9]*[0-9]' was before and it missed py4e.com and etx
-    for number in line :
-        newlist.append(int(number)) # creates newlist with int line values
-
-print(sum(newlist))
+print(sum)
